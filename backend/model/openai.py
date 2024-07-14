5f7bd5380240e42fb86d377ec567acc6af0e1774
Daniel Ogbuigwe <103327095+ogbidaniel@users.noreply.github.com>
2024-07-14 22:58:30 +0100

    add openai api functions. templates

--- backend/model/openai.py
+++ backend/model/openai.py
@@ -1,2 +1,22 @@
 # openai.py
+from pathlib import Path
+from openai import OpenAI
+client = OpenAI()
 
+# transcribe audio
+audio_file= open("/path/to/file/audio.mp3", "rb")
+transcription = client.audio.transcriptions.create(
+  model="whisper-1", 
+  file=audio_file
+)
+print(transcription.text)
+
+# synthesis
+speech_file_path = Path(__file__).parent / "speech.mp3"
+response = client.audio.speech.create(
+  model="tts-1",
+  voice="alloy",
+  input="Today is a wonderful day to build something people love!"
+)
+
+response.stream_to_file(speech_file_path)
